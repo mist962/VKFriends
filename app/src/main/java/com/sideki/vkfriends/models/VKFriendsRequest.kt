b9eba6736eeeba72f98916683431920dd4b77059
Alex <mist962@gmail.com>
2020-12-28 23:45:58 +0300

    Refactoring

---
+++ app/src/main/java/com/sideki/vkfriends/models/VKFriendsRequest.kt
@@ -0,0 +1,23 @@
+package com.sideki.vkfriends.models
+
+import com.vk.api.sdk.requests.VKRequest
+import org.json.JSONObject
+
+class VKFriendsRequest(uid: Int = 0) : VKRequest<List<VKUser>>("friends.get") {
+    init {
+        if (uid != 0) {
+            addParam("user_id", uid)
+        }
+        addParam("lang", 0)
+        addParam("fields", "photo_100")
+    }
+
+    override fun parse(r: JSONObject): List<VKUser> {
+        val users = r.getJSONObject("response").getJSONArray("items")
+        val result = ArrayList<VKUser>()
+        for (i in 0 until users.length()) {
+            result.add(VKUser.parse(users.getJSONObject(i)))
+        }
+        return result
+    }
+}
